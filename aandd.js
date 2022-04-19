96b95bca12108801954f345f170d455134605ab9
DESKTOP-NS6L9BD\shupp <a8120133@gsuite.si.aoyama.ac.jp>
2022-04-19 18:36:15 +0900

    prac1-7とprac1-8

--- aandd.js
+++ aandd.js
@@ -10,3 +10,17 @@ function show(array) {
     //document.getElementById('res').innerHTML = html;
     document.getElementById('res').insertAdjacentHTML('beforeend', html);
 }
+
+function createSequentialNumbers(array, n){
+    for (var i = 1; i < n+1; i++) {
+        array.push(i);
+    }
+    
+}
+
+function createRandomNumbers(array, n){
+    for (var i = 1; i < n+1; i++) {
+        var x = Math.floor(Math.random()*100);
+        array.push(x);
+    }
+}
