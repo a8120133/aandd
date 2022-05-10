2726e0b696ecf4647090a10f65363e96d6e4b431
田名網珠加 <a8120133@gsuite.si.aoyama.ac.jp>
2022-05-10 19:20:26 +0900

    no message

--- aandd.js
+++ aandd.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function show(array) {
     var html = '';
     html = html + '<table border="1">';
@@ -18,6 +17,7 @@ function createSequentialNumbers(array, n) {
     }
 }
 
+//1から100の乱数を生成
 function createRandomNumbers(array, n){
     for (var i = 0; i <= n; i++){
         var num = Math.floor(Math.random()*100);
@@ -25,36 +25,10 @@ function createRandomNumbers(array, n){
     }
 }
 
-function createRandomNumbers20(array, n){ //0から20;
+//0から20の乱数を生成
+function createRandomNumbers20(array, n){ 
     for (var i = 0; i <= n; i++){
         var num = Math.floor(Math.random()*21);
         array.push(num);
     }
-=======
-function show(array) {
-    var html = '';
-    html = html + '<table border="1">';
-    html = html + '<tr>';
-    for (var i = 0; i < array.length; i++) {
-        html = html + '<td>' + array[i] + '</td>';
-    }
-    html = html + '</tr>';
-    html = html + '</table>';
-    //document.getElementById('res').innerHTML = html;
-    document.getElementById('res').insertAdjacentHTML('beforeend', html);
-}
-
-function createSequentialNumbers(array, n){
-    for (var i = 1; i < n+1; i++) {
-        array.push(i);
-    }
-    
-}
-
-function createRandomNumbers(array, n){
-    for (var i = 1; i < n+1; i++) {
-        var x = Math.floor(Math.random()*100);
-        array.push(x);
-    }
->>>>>>> a235c8e58f36871f600608c1877aecf6d1206b72
 }
