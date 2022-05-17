71b98e68323d151a1ca6248894d39c64b3cdb3ba
田名網珠加 <a8120133@gsuite.si.aoyama.ac.jp>
2022-05-17 19:47:21 +0900

    prac4-3, prac4-4

--- aandd.js
+++ aandd.js
@@ -1,14 +1,33 @@
-function show(array) {
-    var html = '';
-    html = html + '<table border="1">';
-    html = html + '<tr>';
-    for (var i = 0; i < array.length; i++) {
-        html = html + '<td>' + array[i] + '</td>';
+// function show(array) {
+//     var html = '';
+//     html = html + '<table border="1">';
+//     html = html + '<tr>';
+//     for (var i = 0; i < array.length; i++) {
+//         html = html + '<td>' + array[i] + '</td>';
+//     }
+//     html = html + '</tr>';
+//     html = html + '</table>';
+//     //document.getElementById('res').innerHTML = html;
+//     document.getElementById('res').insertAdjacentHTML('beforeend', html);
+// }
+
+function show(obj) {
+    if (Array.isArray(obj)) {
+        var html = '';
+        html = html + '<table border="1">';
+        html = html + '<tr>';
+        for (var i = 0; i < obj.length; i++) {
+            html = html + '<td>' + obj[i] + '</td>';
+        }
+        html = html + '</tr>';
+        html = html + '</table>';
+        //html = html + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', html);
+    } else {
+        var string = JSON.stringify(obj);
+        string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', string);
     }
-    html = html + '</tr>';
-    html = html + '</table>';
-    //document.getElementById('res').innerHTML = html;
-    document.getElementById('res').insertAdjacentHTML('beforeend', html);
 }
 
 function createSequentialNumbers(array, n) {
@@ -30,5 +49,19 @@ function createRandomNumbers20(array, n){
     for (var i = 0; i <= n; i++){
         var num = Math.floor(Math.random()*21);
         array.push(num);
+    }
+}
+
+//n個の乱数列を生成する
+function createOrderedRandomNumbers(numbers, n) {
+    var count = 0;
+    for (var i = 1; ; i++) {
+        if (Math.floor(Math.random() * 3) == 0) {
+            numbers.push(i);
+            count++;
+            if (count >= n) {
+                break;
+            }
+        }
     }
 }
